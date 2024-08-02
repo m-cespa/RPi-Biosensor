020e61428b31f72ced2630f8ff69278cd3380580
m-cespa <164089540+m-cespa@users.noreply.github.com>
2024-08-02 13:51:17 +0100

    Update All_Sensors.py

--- All_Sensors.py
+++ All_Sensors.py
@@ -10,6 +10,7 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import adafruit_tca9548a
 import numpy as np
+from tqdm import tqdm
 
 # MULTIPLEXER INITIALISATION
 i2c = busio.I2C(board.SCl, board.SDA)
@@ -44,36 +45,41 @@ split = string.split()
 title = ','.join(split)
 out_file.write(title + '\n')
 
-start = time.time()
-while True:
-# evaluating elapsed time at loop start effectively shifts timings by ~3s
-# this sets the time-scale at regular integer stesps
-    current = time.time()
-    elapsed = current - start
-    if elapsed > (duration+1):
-        print('Data recordng complete. Terminating...')
-        break
-
-    start_loop = time.time()
-
-    out_temps = []
-    for sensor in out_temp_sensors:
-        out_temps.append(sensor.get_temperature())
-# take readings from BME280 & ADS1115 after DS18B20 (faster readings)
-    T_s = [bme.temperature for bme in bmes]
-    P_s = [bme.pressure for bme in bmes]
-    H_s = [bme.humidity for bme in bmes]
-    TPH = T_s + P_s + H_s
-# adc readings for reactors 1&2 (voltages1) and reactors 3&4 (voltages2)
-    voltages1 = [ch.voltage for ch in channels1]
-    voltages2 = [ch.voltage for ch in channels2]
-
-    line = f'{round(elapsed,3)},{",".join(map(str,out_temps))},{",".join(map(str,TPH))},{",".join(map(str,voltages1))},{",".join(map(str,voltages2))}'
-    end_loop = time.time()
-    diff = end_loop - start_loop
-# optional extra pause time (dt is integer by design with no pause time set)
-# pause = 10
-    dt = max(np.ceil(diff),diff) - diff # + pause
-    out_file.write(line + '\n')
-    time.sleep(dt)
+with tqdm(total=duration, desc="Processig: ") as pbar:
+    start = time.time()
+    
+    while True:
+    # evaluating elapsed time at loop start effectively shifts timings by ~3s
+    # this sets the time-scale at regular integer stesps
+        current = time.time()
+        elapsed = current - start
+        if elapsed > (duration+1):
+            print('Data recordng complete. Terminating...')
+            pbar.update(duration - pbar.n)
+            break
+        pbar.update(elapsed - pbar.n)
+        
+        start_loop = time.time()
+    
+        out_temps = []
+        for sensor in out_temp_sensors:
+            out_temps.append(sensor.get_temperature())
+    # take readings from BME280 & ADS1115 after DS18B20 (faster readings)
+        T_s = [bme.temperature for bme in bmes]
+        P_s = [bme.pressure for bme in bmes]
+        H_s = [bme.humidity for bme in bmes]
+        TPH = T_s + P_s + H_s
+    # adc readings for reactors 1&2 (voltages1) and reactors 3&4 (voltages2)
+        voltages1 = [ch.voltage for ch in channels1]
+        voltages2 = [ch.voltage for ch in channels2]
+    
+        line = f'{round(elapsed,3)},{",".join(map(str,out_temps))},{",".join(map(str,TPH))},{",".join(map(str,voltages1))},{",".join(map(str,voltages2))}'
+        end_loop = time.time()
+        diff = end_loop - start_loop
+    # optional extra pause time (dt is integer by design with no pause time set)
+    # pause = 10
+        dt = max(np.ceil(diff),diff) - diff # + pause
+        out_file.write(line + '\n')
+        time.sleep(dt)
+    pbar.close()
 out_file.close()
