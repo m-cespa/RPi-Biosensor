879e83c9fc8c6546ef0ace0a8409975dd6a9c5ad
m-cespa <164089540+m-cespa@users.noreply.github.com>
2024-08-09 14:28:20 +0100

    Update LED_Driver.py

--- LED_Driver.py
+++ LED_Driver.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as IO  # Import the RPi.GPIO module as IO for GPIO pin control
-import time  # Import the time module to provide delays in the program
+import time
 
 # Setup GPIO
 IO.setmode(IO.BOARD)  # Set the GPIO mode to BOARD numbering
@@ -7,7 +7,7 @@ LED_Pin = 35  # Define the GPIO pin number for the LED
 
 # Setup LED pin
 IO.setup(LED_Pin, IO.OUT)  # Set the LED_Pin as an output pin
-p = IO.PWM(LED_Pin, 5000)  # Initialize PWM on LED_Pin with a frequency of 5000 Hz (5 kHz)
+p = IO.PWM(LED_Pin, 5000)  # Initialize PWM on LED_Pin with a frequency of 5000 Hz
 p.start(0)  # Start PWM with a duty cycle of 0%
 
 try:
