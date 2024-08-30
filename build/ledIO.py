e22100b704897bdfe497e1bc0ea63e973eb92e8c
Michele Cespa <michele.cespa@gmail.com>
2024-08-30 11:25:10 +0100

    updated ledIO.py

--- build/ledIO.py
+++ build/ledIO.py
@@ -1,24 +1,40 @@
 import RPi.GPIO as IO
 import time
 
+bcm = {7:4, 11:17, 12:18, 13:27, 15:22, 16:23, 18:24, 22:25, 
+    29:5, 31:6, 32:12, 33:13, 35:19, 36:16, 37:26, 38:20, 40:21}
+
 class LED_IO:
-    """"""
-    def __init__(self, pin: int, board_mode: str):
+    def __init__(self, board_mode:str, pin:int):
         """GPIO operated LED class with simple top_hat and finish methods"""
-        self.pin = pin
         self.board_mode = board_mode.upper()
+        self.pin = pin
         if self.board_mode == 'BOARD':
             IO.setmode(IO.BOARD)
         elif self.board_mode == 'BCM':
+            self.pin = bcm[self.pin]
             IO.setmode(IO.BCM)
-        IO.setup(self.pin, IO.out)
+        else:
+            raise ValueError("Invalid board mode: use 'BCM' or 'BOARD'")
+        IO.setup(self.pin, IO.OUT)
         IO.output(self.pin, 0)
+        
 
     def top_hat(self, duration: float):
         IO.output(self.pin, 1)
         time.sleep(duration)
         IO.output(self.pin, 0)
+        
+    def on(self):
+        IO.output(self.pin, 1)
+        
+    def off(self):
+        IO.output(self.pin, 0)
 
     def finish(self):
         IO.output(self.pin, 0)
         IO.cleanup()
+        
+led = LED_IO('bcm', 38)
+led.top_hat(10)
+led.finish()
